ccab7e6ecb37f8825b60a5a6c87da04d4f696ca2
Kirill Sadovnikov <d4rkm3z@gmail.com>
2021-04-12 14:40:16 +0300

    chore: rule of autosort is added to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
+const localePrettierConfig = require('./.prettierrc.js');
+
 module.exports = {
   extends: ['react-app', 'react-app/jest', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['simple-import-sort', 'prettier'],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'comma-dangle': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'simple-import-sort/imports': 'warn',
+    'prettier/prettier': ['error', localePrettierConfig],
+  },
   overrides: [
     {
       files: ['**/*.ts?(x)'],
@@ -14,6 +26,21 @@ module.exports = {
               regex: '^I[A-Z]',
               match: true,
             },
+          },
+        ],
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              [
+                '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+              ],
+              ['^react', '^@?\\w'],
+              ['^\\u0000'],
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              ['^.+\\.s?css$'],
+            ],
           },
         ],
       },
